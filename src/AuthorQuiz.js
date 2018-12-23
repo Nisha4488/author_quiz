4d7276646073317fca4c269233fa8673bd3b290c
Nisha Bharti <me.nishabharti@gmail.com>
2018-12-23 10:30:51 -0700

    generated random authors

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -16,9 +16,37 @@ const authors = [
     books: [
     'Wonderland The Adventures of Sherlock Holmes',
     'The Call of the Wild',
+    ]
+  },
+  {
+    name: 'Joseph Conrad',
+    imageUrl: 'images/authors/JosephConrad.jpg',
+    imageSource:' Wikimedia Commons',
+    books: [
+    'Notes on My Books',
+    'Twelve Steps and Twelve Traditions'
+    ]
+  },
+  {
+    name: 'Stephen King',
+    imageUrl: 'images/authors/StephenKing.jpg',
+    imageSource:' Wikimedia Commons',
+    books: [
+    'The Bachman Books',
+    'Strange Highways',
+    ]
+  },
+  {
+    name: 'William Shakespeare',
+    imageUrl: 'images/authors/WilliamShakespeare.jpg',
+    imageSource:' Wikimedia Commons',
+    books: [
+    'The Story of Gulliver',
+    'The Story of Crime and Punishment',
     'Jack London',
     ]
-  }
+  },
+
 ]
 
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Author from './Author'
 
 const Main =({authors})=>{
-
-  
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  const author = authors[getRandomInt(authors.length-1)];
   return(
-    <div >
-      {authors.map(author=><Author key={author.name} author={author} />)}
+    <div  >
+      <Author key={author.name} author={author} />
     </div>
   )
 }
